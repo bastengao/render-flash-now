b1dffe73fa87445d7eab493b0fdad69e1ac6d676
bastengao <bastengao@gmail.com>
2016-09-06 10:10:31 +0800

    Replace alias_method_chain with prepend

--- lib/render_flash_now.rb
+++ lib/render_flash_now.rb
@@ -2,4 +2,4 @@ require 'render_flash_now/version'
 require 'render_flash_now/render_flash'
 require 'active_support/concern'
 
-ActionController::Base.send :include, RenderFlashNow::RenderFlash
+ActionController::Base.prepend RenderFlashNow::RenderFlash
--- lib/render_flash_now/render_flash.rb
+++ lib/render_flash_now/render_flash.rb
@@ -1,12 +1,7 @@
 module RenderFlashNow
   module RenderFlash
-    def self.included(base)
-      base.class_eval do
-        alias_method_chain :render, :flash
-      end
-    end
 
-    def render_with_flash(*args)
+    def render(*args)
       if options = args[1]
         self.class._flash_types.each do |flash_type|
           if type = options.delete(flash_type)
@@ -15,7 +10,7 @@ module RenderFlashNow
         end
       end
 
-      render_without_flash(*args)
+      super
     end
   end
 end
